cbec87460018d2767f2111e8bc729232aeb1b581
Leo <lhtshent@gmail.com>
2024-11-05 11:18:07 +0800

    add order test case

--- src/main/java/com/lht/lhtsharding/LhtshardingApplication.java
+++ src/main/java/com/lht/lhtsharding/LhtshardingApplication.java
@@ -1,6 +1,8 @@
 package com.lht.lhtsharding;
 
 import com.lht.lhtsharding.config.ShardingAutoConfiguration;
+import com.lht.lhtsharding.demo.mapper.OrderMapper;
+import com.lht.lhtsharding.demo.model.Order;
 import com.lht.lhtsharding.demo.model.User;
 import com.lht.lhtsharding.demo.mapper.UserMapper;
 import com.lht.lhtsharding.mybatis.ShardingMapperFactoryBean;
@@ -24,19 +26,35 @@ public class LhtshardingApplication {
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+    OrderMapper orderMapper;
+
     @Bean
     ApplicationRunner runner() {
         return x -> {
 
+            System.out.println(" ================> ================> ================>");
+            System.out.println(" ================> test user sharding ================>");
+            System.out.println(" ================> ================> ================>");
+            for (int i = 1; i <= 60; i++) {
+                testUser(i);
+            }
+
+            System.out.println("\n\n\n\n");
+
+            System.out.println(" ================> ================> ================>");
+            System.out.println(" ================> test order sharding ================>");
+            System.out.println(" ================> ================> ================>");
 
             for (int i = 1; i <= 60; i++) {
-                test(i);
+                testOrder(i);
             }
+
 
         };
     }
 
-    private void test(int id) {
+    private void testUser(int id) {
         System.out.println(" ======> 1. test insert....");
         int insert = userMapper.insert(new User(id, "leo" + id, 18));
         System.out.println(" ======> insert =" + insert);
@@ -53,6 +71,46 @@ public class LhtshardingApplication {
         System.out.println(" ======> 5. test delete....");
         int delete = userMapper.deleteById(id);
         System.out.println(" ======> delete =" + delete);
+    }
+
+    private void testOrder(int id) {
+        int id2 = id + 100;
+        System.out.println("\n\n ====================>> id = " + id);
+        System.out.println(" ======> 1. test insert....");
+        int insert = orderMapper.insert(new Order(id, 1, 18));
+        System.out.println(" ======> insert =" + insert);
+        int inserted = orderMapper.insert(new Order(id2, 2, 18));
+        System.out.println(" ======> insert =" + inserted);
+
+
+        System.out.println(" ======> 2. test find....");
+        Order order = orderMapper.findById(id, 1);
+        System.out.println(" ======> result =" + order);
+        Order order1 = orderMapper.findById(id2, 2);
+        System.out.println(" ======> result =" + order1);
+
+
+        System.out.println(" ======> 3. test update....");
+        order.setPrice(11d);
+        int update = orderMapper.update(order);
+        System.out.println(" ======> update =" + update);
+        order1.setPrice(22d);
+        int updated = orderMapper.update(order1);
+        System.out.println(" ======> update =" + updated);
+
+
+        System.out.println(" ======> 4. test new find....");
+        Order order11 = orderMapper.findById(id, 1);
+        System.out.println(" ======> result =" + order11);
+        Order order22 = orderMapper.findById(id2, 2);
+        System.out.println(" ======> result =" + order22);
+
+
+        System.out.println(" ======> 5. test delete....");
+        int delete = orderMapper.deleteById(id, 1);
+        System.out.println(" ======> delete =" + delete);
+        int deleted = orderMapper.deleteById(id2, 2);
+        System.out.println(" ======> delete =" + deleted);
     }
 
 }
